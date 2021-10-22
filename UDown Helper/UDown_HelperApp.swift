2091ebed417db9b1ada6aa07f1a7f8306fe0d837
Derek Jing <jingdian@magicdian.cn>
2021-10-22 10:13:22 +0800

    Initial Commit

---
+++ UDown Helper/UDown_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDown_HelperApp.swift
+//  UDown Helper
+//
+//  Created by Derek Jing on 2021/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UDown_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
